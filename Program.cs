dcf11c737853dbcd910abd3dd5b90bbe1b1aa998
ThinkPad T14 <ThinkPad T14@DESKTOP-C9FJOHD>
2024-07-04 11:53:13 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Latih9_ProsedurFungsi
+{
+    internal class Program
+    {
+
+        static void TglSkrg() // Prosedur menampilkan Tanggal Sekarang
+        {
+            var Skrg = DateTime.Now;
+            Console.WriteLine($"Tanggal Sekarang Adalah = {Skrg}");
+        }
+
+        static int Kali(int a, int b) // Function kali
+        {
+            return a * b;
+        }
+
+        static bool apakahGenap(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+           
+        }
+        static void Main(string[] args)
+        {
+            TglSkrg(); // Iki Memanggil Prosedur ya:)
+            Console.WriteLine(Kali(4,6)); // Ini memanggil Function Kali
+            Console.WriteLine(apakahGenap(7)); // ini cek bilangan
+
+        }
+    }
+}
